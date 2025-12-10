0656b35c119dfaeed6fdb3866199d51f1ba11202
Alloi Stefano (SFM KAI SDM) <stefano.alloi@k-ai.at>
2025-12-10 17:00:50 +0100

    solution game 9

---
+++ Game9.py
@@ -0,0 +1,613 @@
+#day 9
+
+input_list = """97538,50203
+97538,51426
+98142,51426
+98142,52612
+97484,52612
+97484,53823
+97514,53823
+97514,55016
+97318,55016
+97318,56330
+98062,56330
+98062,57447
+97258,57447
+97258,58648
+97093,58648
+97093,59893
+97115,59893
+97115,61042
+96677,61042
+96677,62143
+96088,62143
+96088,63420
+96155,63420
+96155,64603
+95854,64603
+95854,65835
+95687,65835
+95687,66990
+95280,66990
+95280,68166
+94919,68166
+94919,69282
+94406,69282
+94406,70411
+93926,70411
+93926,71243
+92843,71243
+92843,72478
+92586,72478
+92586,73456
+91836,73456
+91836,74615
+91409,74615
+91409,75584
+90657,75584
+90657,76552
+89915,76552
+89915,77844
+89642,77844
+89642,78395
+88324,78395
+88324,79315
+87539,79315
+87539,80580
+87178,80580
+87178,81529
+86410,81529
+86410,82509
+85674,82509
+85674,82923
+84339,82923
+84339,84306
+84013,84306
+84013,84794
+82792,84794
+82792,85473
+81774,85473
+81774,86454
+81024,86454
+81024,87005
+79910,87005
+79910,87936
+79106,87936
+79106,88489
+78009,88489
+78009,89900
+77518,89900
+77518,89813
+75995,89813
+75995,90439
+74966,90439
+74966,91596
+74247,91596
+74247,91847
+72993,91847
+72993,92915
+72183,92915
+72183,92820
+70777,92820
+70777,93543
+69781,93543
+69781,93814
+68580,93814
+68580,94509
+67558,94509
+67558,94940
+66425,94940
+66425,95524
+65343,95524
+65343,95910
+64188,95910
+64188,96433
+63071,96433
+63071,96383
+61797,96383
+61797,96930
+60678,96930
+60678,97267
+59502,97267
+59502,97014
+58218,97014
+58218,97418
+57056,97418
+57056,97545
+55848,97545
+55848,97940
+54667,97940
+54667,98215
+53464,98215
+53464,97972
+52226,97972
+52226,98462
+51020,98462
+51020,97596
+49796,97596
+49796,98190
+48572,98190
+48572,97953
+47362,97953
+47362,97708
+46161,97708
+46161,97340
+44980,97340
+44980,97672
+43720,97672
+43720,97602
+42498,97602
+42498,97635
+41251,97635
+41251,96971
+40137,96971
+40137,96299
+39046,96299
+39046,96221
+37820,96221
+37820,95752
+36696,95752
+36696,95700
+35446,95700
+35446,95059
+34382,95059
+34382,95005
+33112,95005
+33112,94527
+31991,94527
+31991,93909
+30933,93909
+30933,93570
+29753,93570
+29753,93088
+28634,93088
+28634,92294
+27676,92294
+27676,91830
+26547,91830
+26547,91631
+25252,91631
+25252,90640
+24426,90640
+24426,90003
+23388,90003
+23388,89287
+22404,89287
+22404,88586
+21410,88586
+21410,88159
+20199,88159
+20199,87419
+19220,87419
+19220,85943
+18875,85943
+18875,85540
+17613,85540
+17613,84367
+17050,84367
+17050,83516
+16194,83516
+16194,83066
+14914,83066
+14914,82125
+14134,82125
+14134,80962
+13619,80962
+13619,80062
+12807,80062
+12807,79174
+11974,79174
+11974,78391
+10986,78391
+10986,77011
+10835,77011
+10835,76454
+9482,76454
+9482,75279
+9052,75279
+9052,74391
+8155,74391
+8155,73316
+7563,73316
+7563,71947
+7541,71947
+7541,70783
+7167,70783
+7167,69876
+6247,69876
+6247,68749
+5786,68749
+5786,67760
+4978,67760
+4978,66442
+5014,66442
+5014,65417
+4256,65417
+4256,64037
+4579,64037
+4579,62971
+3921,62971
+3921,61741
+3835,61741
+3835,60669
+3108,60669
+3108,59441
+3036,59441
+3036,58262
+2734,58262
+2734,57112
+2205,57112
+2205,55852
+2424,55852
+2424,54609
+2654,54609
+2654,53466
+1746,53466
+1746,52248
+1563,52248
+1563,51011
+1947,51011
+1947,50191
+94817,50191
+94817,48573
+1861,48573
+1861,47338
+1622,47338
+1622,46159
+2267,46159
+2267,44929
+2178,44929
+2178,43713
+2273,43713
+2273,42549
+2720,42549
+2720,41243
+2318,41243
+2318,40151
+3096,40151
+3096,39051
+3719,39051
+3719,37655
+3151,37655
+3151,36639
+4052,36639
+4052,35418
+4214,35418
+4214,34214
+4457,34214
+4457,33117
+5008,33117
+5008,32061
+5644,32061
+5644,30954
+6139,30954
+6139,29568
+6030,29568
+6030,28858
+7363,28858
+7363,27740
+7828,27740
+7828,26650
+8354,26650
+8354,25366
+8561,25366
+8561,24452
+9401,24452
+9401,23445
+10081,23445
+10081,22480
+10821,22480
+10821,21213
+11147,21213
+11147,20587
+12336,20587
+12336,19758
+13233,19758
+13233,18267
+13354,18267
+13354,17974
+14856,17974
+14856,17062
+15645,17062
+15645,15787
+16079,15787
+16079,14773
+16801,14773
+16801,14112
+17855,14112
+17855,13302
+18768,13302
+18768,12982
+20079,12982
+20079,11547
+20497,11547
+20497,11435
+21935,11435
+21935,10891
+23027,10891
+23027,9506
+23560,9506
+23560,9102
+24750,9102
+24750,8710
+25931,8710
+25931,7651
+26731,7651
+26731,7576
+28070,7576
+28070,6546
+28915,6546
+28915,6301
+30147,6301
+30147,5902
+31299,5902
+31299,5625
+32494,5625
+32494,4822
+33487,4822
+33487,4507
+34667,4507
+34667,3753
+35707,3753
+35707,4109
+37081,4109
+37081,3314
+38125,3314
+38125,2795
+39258,2795
+39258,3372
+40625,3372
+40625,2956
+41776,2956
+41776,2438
+42922,2438
+42922,2576
+44166,2576
+44166,2191
+45345,2191
+45345,1742
+46532,1742
+46532,2067
+47774,2067
+47774,1719
+48983,1719
+48983,1575
+50207,1575
+50207,2222
+51415,2222
+51415,1987
+52641,1987
+52641,2283
+53839,2283
+53839,2387
+55047,2387
+55047,2851
+56210,2851
+56210,2229
+57527,2229
+57527,3273
+58581,3273
+58581,3241
+59818,3241
+59818,3381
+61028,3381
+61028,4020
+62115,4020
+62115,4176
+63324,4176
+63324,4227
+64576,4227
+64576,4447
+65788,4447
+65788,5464
+66711,5464
+66711,5545
+67978,5545
+67978,6118
+69054,6118
+69054,6909
+70022,6909
+70022,7281
+71182,7281
+71182,7722
+72314,7722
+72314,8143
+73467,8143
+73467,8789
+74497,8789
+74497,9588
+75429,9588
+75429,10264
+76432,10264
+76432,10907
+77458,10907
+77458,11773
+78323,11773
+78323,12347
+79403,12347
+79403,12963
+80464,12963
+80464,13867
+81288,13867
+81288,14675
+82190,14675
+82190,15373
+83198,15373
+83198,16101
+84189,16101
+84189,17414
+84575,17414
+84575,17667
+86096,17667
+86096,19064
+86349,19064
+86349,19964
+87159,19964
+87159,20504
+88444,20504
+88444,21618
+88999,21618
+88999,22490
+89887,22490
+89887,23738
+90220,23738
+90220,24762
+90879,24762
+90879,25963
+91234,25963
+91234,26827
+92172,26827
+92172,28135
+92298,28135
+92298,29188
+92891,29188
+92891,29984
+94058,29984
+94058,31387
+93891,31387
+93891,32538
+94262,32538
+94262,33568
+94955,33568
+94955,34519
+95930,34519
+95930,35811
+95910,35811
+95910,36972
+96277,36972
+96277,38287
+96048,38287
+96048,39460
+96320,39460
+96320,40499
+97254,40499
+97254,41813
+96830,41813
+96830,42872
+97894,42872
+97894,44095
+98004,44095
+98004,45379
+97458,45379
+97458,46577
+97636,46577
+97636,47795
+97496,47795
+97496,48991
+97894,48991
+97894,50203"""
+
+#parse input
+input_lines = input_list.splitlines()
+coords = [[0,0] for i in range(len(input_lines))]
+for i in range(len(input_lines)):
+    coords[i][0] = int(input_lines[i].split(",")[0])
+    coords[i][1] = int(input_lines[i].split(",")[1])
+
+# solution part 1
+areas = [[0,0,0] for i in range(int(len(coords)*(len(coords)-1)/2))]
+k=0
+for i in range(len(coords)):
+    for j in range(i+1, len(coords)):
+        areas[k][0] = i
+        areas[k][1] = j
+        areas[k][2] = (abs(coords[i][0]-coords[j][0])+1)*(abs(coords[i][1]-coords[j][1])+1)
+        k = k+1
+
+areas.sort(key=lambda x: x[2])
+print("Solution part 1: ", areas[-1][2])
+
+
+# solution part 2
+look_for_y = True
+h_segments = []
+v_segments = []
+
+# create list of horizontal and vertical segments.
+# First value is, respectively, the vertical and horizontal position,
+# Second value is starting point
+# Third value is end point
+for i in range(len(coords)):
+    for j in range(i+1, len(coords)):
+        if look_for_y:
+            if coords[i][1] == coords[j][1]:
+                segment = [coords[i][1], min(coords[i][0], coords[j][0]), max(coords[i][0], coords[j][0])]
+                h_segments.append(segment)
+                look_for_y = False
+        else:
+            if coords[i][0] == coords[j][0]:
+                segment = [coords[i][0], min(coords[i][1], coords[j][1]), max(coords[i][1], coords[j][1])]
+                v_segments.append(segment)
+                look_for_y = True
+
+# rectangles_without_inner_points will contain only rectangles which don't have points in their inside.
+# [[top_left corner], [top-right corner], [bottom-left corner], [bottom-right corner], area]
+# But maybe they have points on the borders, or they are entirely crossed by lines with points outside
+rectangles_without_inner_points = []
+for i in range(len(coords)):
+    for j in range(i+1, len(coords)):
+        if coords[i][0] <= coords[j][0]:
+            left_border = coords[i][0]
+            right_border = coords[j][0]
+        else:
+            left_border = coords[j][0]
+            right_border = coords[i][0]
+        if coords[i][1] <= coords[j][1]:
+            top_border = coords[i][1]
+            bottom_border = coords[j][1]
+        else:
+            top_border = coords[j][1]
+            bottom_border = coords[i][1]
+        found_point_inside_rectangle = False
+        for k in range(len(coords)):
+            if k != i and k != j:
+                if left_border < coords[k][0] < right_border and top_border < coords[k][1] < bottom_border:
+                    found_point_inside_rectangle = True
+                    break
+        if not found_point_inside_rectangle:
+            rectangle = [[left_border, top_border],[right_border, top_border], [left_border, bottom_border], [right_border, bottom_border],(abs(coords[i][0]-coords[j][0])+1)*(abs(coords[i][1]-coords[j][1])+1)]
+            rectangles_without_inner_points.append(rectangle)
+
+rectangles_without_intersections = []
+for i in range(len(rectangles_without_inner_points)):
+    discard_rectangle = False
+    for j in range(len(h_segments)):
+        # horizontal segment j can intersect vertical segments of rectangle i
+        if rectangles_without_inner_points[i][0][1] < h_segments[j][0] < rectangles_without_inner_points[i][2][1]:
+            #horizontal segment is intersecting the vertical segments of rectangle i!
+            if h_segments[j][1] <= rectangles_without_inner_points[i][0][0] and h_segments[j][2] >= rectangles_without_inner_points[i][1][0]:
+                # go to next rectangle
+                discard_rectangle = True
+                break
+    if not discard_rectangle:
+        for j in range(len(v_segments)):
+            # vertical  segment j can intersect horizontal segments of rectangle i
+            if rectangles_without_inner_points[i][0][0] < v_segments[j][0] < rectangles_without_inner_points[i][1][0]:
+                #vertical segment is intersecting the horizontal segments of rectangle i!
+                if v_segments[j][1] <= rectangles_without_inner_points[i][0][1] and v_segments[j][2] >= rectangles_without_inner_points[i][2][1]:
+                    # go to next rectangle
+                    discard_rectangle = True
+                    break
+    if not discard_rectangle:
+        rectangles_without_intersections.append(rectangles_without_inner_points[i])
+
+rectangles_inside_shape = []
+for i in range(len(rectangles_without_intersections)):
+    #create a test segment from a point inside the rectangle
+    #let's do it vertical. It will start from y=0 down to the midpoint of the rectangle
+    v_test_segment = [rectangles_without_intersections[i][0][0]+(rectangles_without_intersections[i][1][0]-rectangles_without_intersections[i][0][0])/2, 0, rectangles_without_intersections[i][0][1]+(rectangles_without_intersections[i][2][1]-rectangles_without_intersections[i][0][1])/2]
+    #let's check how many horizontal segments of the shape it will intersect. If the number is even, the rectangle i is outside the shape
+    num_intersections = 0
+    for j in range(len(h_segments)):
+        if h_segments[j][1] < v_test_segment[0] < h_segments[j][2]:
+            if v_test_segment[1] < h_segments[j][0] < v_test_segment[2]:
+                num_intersections += 1
+    if num_intersections%2 == 1:
+        rectangles_inside_shape.append(rectangles_without_intersections[i])
+
+rectangles_inside_shape.sort(key=lambda x: x[4])
+print("Solution2: ", rectangles_inside_shape[-1][4])
+
+
+
+
